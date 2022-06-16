9a101677c7334cd4876a39bf9e9a9745c1a251b4
AugustoTI <augustocesarti2021@gmail.com>
2022-06-16 20:47:10 -0300

    Adding error checking interface

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPath from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPath()],
+  plugins: [react(), svgr(), tsconfigPath(), checker({ typescript: true })],
   test: {
     globals: true,
     environment: 'jsdom',
