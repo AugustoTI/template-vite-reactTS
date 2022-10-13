6c702153671d05cc7e43a83d8e4a06ad94b2499e
AugustoTI <augustocesarti2021@gmail.com>
2022-10-13 14:24:18 -0300

    updating packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vitest/config';
-import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPath from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPath(), checker({ typescript: true })],
+  plugins: [react(), svgr(), tsconfigPath()],
   test: {
     globals: true,
     environment: 'jsdom',
